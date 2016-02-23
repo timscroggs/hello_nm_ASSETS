1726bae39883ea3979deef54acb9a5ea42b68a0a
Tim Scroggs <t@hnycmbcreative.com>
2016-02-23 01:23:38 -0500

    Added style switcher, adjusted vimeo player to be full-screen, and other responsive css edits.

--- _site/scripts/typed.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$("#typed").typed({
-		strings: ["an ASTRONAUT, JK","a 3D ANIMATOR","a CODER","a DIGITAL EDITOR", "a VIDEO DIRECTOR", "a MUSICIAN", "a UX/UI DESIGNER", "a GRAPHIC DESIGNER","an EDUCATOR"],
-		typeSpeed: 100,
-    backDelay: 1500,
-    loop: true,
-    callback: function(){}
-	});
-});
